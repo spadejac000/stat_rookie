cc1a89a7b1f81100ed004587a4de19968a853026
Jacob Spade <spade.jacob@Yahoo.com>
2019-02-25 16:03:46 -0800

    Updates

---
+++ precache-manifest.3486898e4a4091e251e0ff5d07efb4d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1781bb64586835a46ae5",
+    "url": "/stat_rookie/static/css/main.432c04c0.chunk.css"
+  },
+  {
+    "revision": "1781bb64586835a46ae5",
+    "url": "/stat_rookie/static/js/main.1781bb64.chunk.js"
+  },
+  {
+    "revision": "4bfd02f44a1c1c90d20b",
+    "url": "/stat_rookie/static/js/runtime~main.4bfd02f4.js"
+  },
+  {
+    "revision": "28ad1c4f4fcf4098040f",
+    "url": "/stat_rookie/static/css/2.2374a4bf.chunk.css"
+  },
+  {
+    "revision": "28ad1c4f4fcf4098040f",
+    "url": "/stat_rookie/static/js/2.28ad1c4f.chunk.js"
+  },
+  {
+    "revision": "ca1d24bdaae5e3ed3626a90e4f7f5f61",
+    "url": "/stat_rookie/index.html"
+  }
+];
