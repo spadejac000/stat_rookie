5930ddce24ceefaa93c7af5a66c37fdfbfb9a196
Jacob Spade <spade.jacob@Yahoo.com>
2019-02-26 09:13:12 -0800

    added contact info

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -17,12 +17,23 @@ class Footer extends Component {
         <Container>
           <div className="text-right">
             <span>Contact Us</span>
-            <a href="https://twitter.com/">
+            <a href="https://twitter.com/statrookie">
               <i 
                 className="fab fa-twitter-square" 
                 style={{
                   marginLeft: "8px",
-                  color: "#38A1F3"
+                  color: "#38A1F3",
+                  fontSize: '24px'
+                }}>
+              </i>
+            </a>
+            <a href="info@statrookie.com">
+              <i 
+                className="fas fa-envelope-square" 
+                style={{
+                  marginLeft: "8px",
+                  color: "#d44638",
+                  fontSize: '24px'
                 }}>
               </i>
             </a>
